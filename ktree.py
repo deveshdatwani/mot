50425ae4064779096a29167852151f26f70c66c2
deveshdatwani <datwanidevesh@gmail.com>
2026-03-02 00:49:10 -0500

    building kd tree

--- ktree.py
+++ ktree.py
@@ -1,27 +1,18 @@
 import numpy as np
 
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, left=None, right=None):
         self.data = data
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
-np.random.seed(1)
-x = np.random.randint(size=(123,3), low=0, high=100)
-
-def insert(root, x, depth):
-    if root is None:
-        return Node(x)
-    elif root.data[depth % x.shape[-1]] > x[depth % x.shape[-1]]:
-        root.left = insert(root.left, x, depth+1)
-    else: 
-        root.right = insert(root.right, x, depth+1)
-    return root
-
-def build_tree(x):
-    main_root = Node(x[0])
-    for x_ in x[1:]:
-        insert(main_root, x_, 0)
-    return main_root
-
-x_root = build_tree(x)
+    
+def build_kdtree(points, depth=0):
+    if len(points) == 0:
+        return None
+    axis = depth % points.shape[1]
+    points = points[points[:, axis].argsort()]
+    median  = len(points) // 2
+    return Node(points[median],
+                build_kdtree(points[:median], depth + 1),
+                build_kdtree(points[median + 1:], depth + 1))
