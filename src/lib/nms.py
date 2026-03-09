6a9c621f51425acfcf5b5376e1d1f9cc730f34ed
deveshdatwani <datwanidevesh@gmail.com>
2026-03-09 12:04:59 -0400

    nms indexing changes

--- src/lib/nms.py
+++ src/lib/nms.py
@@ -12,16 +12,15 @@ def nms(bboxes):
     order = scores.argsort()[::-1]
     keep = []
     while order.size > 0:
-        i = order[0]
-        keep.append(i)
-        xx1 = np.maximum(x1[i], x1[order[1:]])
-        yy1 = np.maximum(y1[i], y1[order[1:]])
-        xx2 = np.minimum(x2[i], x2[order[1:]])
-        yy2 = np.minimum(y2[i], y2[order[1:]])
+        keep.append(order[0])
+        xx1 = np.maximum(x1[order[0]], x1[order[1:]])
+        yy1 = np.maximum(y1[order[0]], y1[order[1:]])
+        xx2 = np.minimum(x2[order[0]], x2[order[1:]])
+        yy2 = np.minimum(y2[order[0]], y2[order[1:]])
         w = np.maximum(0, xx2 - xx1)
         h = np.maximum(0, yy2 - yy1)
         inter = w * h
-        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        ovr = inter / (areas[order[0]] + areas[order[1:]] - inter)
         inds = np.where(ovr <= 0.5)[0]
         order = order[inds + 1]
     return keep
